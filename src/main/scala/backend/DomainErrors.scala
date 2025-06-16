c4295cd3c9672ea33311127d06cf21fbc9b7cbeb
TheBugYouCantFix <alex.gorbanev05@gmail.com>
2025-06-17 01:24:19 +0300

    fix: rewrite error tree using enums

--- src/main/scala/backend/DomainErrors.scala
+++ src/main/scala/backend/DomainErrors.scala
@@ -1,20 +1,10 @@
 package backend
 
-import io.circe.{Decoder, Encoder}
-import io.circe.derivation.{ConfiguredEncoder, ConfiguredDecoder, Configuration}
-
-given Configuration = Configuration.default.withDiscriminator("type")
-given Encoder[ErrorResponse] = ConfiguredEncoder.derived
-given Decoder[ErrorResponse] = ConfiguredDecoder.derived
-
 sealed trait ErrorResponse:
-  def message: String
+  val message: String
 
-sealed trait IngredientError extends ErrorResponse
-sealed trait StorageError extends ErrorResponse
+enum IngredientError(val message: String) extends ErrorResponse:
+  case IngredientNotFound(ingredientId: IngredientId) extends IngredientError(s"No ingredient with id $ingredientId")
 
-final case class IngredientNotFound(id: IngredientId) extends IngredientError:
-  override def message: String = s"Ingredient with id = $id is not found"
-
-final case class StorageNotFound(id: StorageId) extends StorageError:
-  override def message: String = s"Storage with id = $id is not found"
+enum StorageError(val message: String) extends ErrorResponse:
+  case StorageNotFound(storageId: StorageId) extends StorageError(s"No storage with id $storageId")
