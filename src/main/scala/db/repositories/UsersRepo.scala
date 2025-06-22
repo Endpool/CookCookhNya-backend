7e0a6e78a52e2ac2d2ad453166ba1657464cd549
danielambda <daniel.gevorgyan25@gmail.com>
2025-06-23 00:06:15 +0300

    refactor: simplified saveUser implementation using magnum builtins

--- src/main/scala/db/repositories/UsersRepo.scala
+++ src/main/scala/db/repositories/UsersRepo.scala
@@ -13,12 +13,9 @@ final case class UsersRepoLive(xa: Transactor) extends Repo[Users, Users, UserId
   def saveUser(userId: UserId, alias: Option[String], fullName: String): IO[DbError.UnexpectedDbError, Unit] =
     val user = Users(userId, alias, fullName)
     xa.transact {
-      val existent: Vector[Users] = findAll(Spec[Users]
-        .where(sql"${Users.table.id} = ${user.id}")
-        .limit(1)
-      )
-      if existent.isEmpty then insert(user) else update(user)
-
+      if existsById(user.id)
+        then insert(user)
+        else update(user)
     }.catchAll(e => ZIO.fail(DbError.UnexpectedDbError(e.getMessage)))
 
 object UsersRepo:
