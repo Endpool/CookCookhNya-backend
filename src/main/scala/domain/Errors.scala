0dbd326e42b61754bbfa89080499fa60cf8d04d9
TheBugYouCantFix <alex.gorbanev05@gmail.com>
2025-06-22 17:07:18 +0300

    feat: add recipe error (for future use)

--- src/main/scala/domain/Errors.scala
+++ src/main/scala/domain/Errors.scala
@@ -12,5 +12,8 @@ enum StorageError(val message: String) extends ErrorResponse:
 enum UserError(val message: String) extends ErrorResponse:
   case NotFound(userId: UserId) extends UserError(s"No user with id $userId")
 
+enum RecipeError(val message: String) extends ErrorResponse:
+  case NotFound(recipeId: RecipeId) extends RecipeError(s"No user with id $RecipeError")
+
 enum DbError(val message: String) extends ErrorResponse:
   case UnexpectedDbError(msg: String) extends DbError(s"Something went wrong with the db: $msg")
