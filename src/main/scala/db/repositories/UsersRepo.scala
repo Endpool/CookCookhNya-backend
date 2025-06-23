30caeb72343c645e3822c06d8aa24053368ac431
danielambda <daniel.gevorgyan25@gmail.com>
2025-06-24 00:35:37 +0300

    fix: simplified user upsertion

--- src/main/scala/db/repositories/UsersRepo.scala
+++ src/main/scala/db/repositories/UsersRepo.scala
@@ -7,19 +7,18 @@ import com.augustnagro.magnum.magzio.*
 import zio.{IO, RLayer, UIO, ZIO, ZLayer}
 
 trait UsersRepo:
-  def saveUser(userId: UserId, alias: Option[String], fullName: String): IO[DbError.UnexpectedDbError, Unit]
+  def saveUser(userId: UserId, alias: Option[String], fullName: String):
+    IO[DbError.UnexpectedDbError, Unit]
 
 final case class UsersRepoLive(xa: Transactor) extends Repo[DbUser, DbUser, UserId] with UsersRepo:
-  def saveUser(userId: UserId, alias: Option[String], fullName: String): IO[DbError.UnexpectedDbError, Unit] =
+  def saveUser(userId: UserId, alias: Option[String], fullName: String):
+    IO[DbError.UnexpectedDbError, Unit] =
     val user = DbUser(userId, alias, fullName)
     xa.transact {
-      val existent: Vector[DbUser] = findAll(Spec[DbUser]
-        .where(sql"${usersTable.id} = ${user.id}")
-        .limit(1)
-      )
-      if existent.isEmpty then insert(user) else update(user)
-
-    }.catchAll(e => ZIO.fail(DbError.UnexpectedDbError(e.getMessage)))
+      if existsById(user.id)
+        then insert(user)
+        else update(user)
+    }.mapError(e => DbError.UnexpectedDbError(e.getMessage))
 
 object UsersRepo:
   val layer = ZLayer.fromFunction(UsersRepoLive(_))
