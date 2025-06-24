64e6128f07f5af65710157fb5ec9c1dddfa369b3
Leropsis <v.ksenofontov@innopolis.university>
2025-06-25 01:50:29 +0300

    chore: add database error handling for storage ingredients repo

--- src/main/scala/db/repositories/StorageIngredientsRepo.scala
+++ src/main/scala/db/repositories/StorageIngredientsRepo.scala
@@ -1,45 +1,47 @@
 package db.repositories
 
 import db.tables.{DbStorageIngredient, storageIngredientsTable}
-import domain.{IngredientId, StorageError, IngredientError, StorageId, DbError}
+import domain.{IngredientId, StorageError, StorageId, DbError}
+import domain.DbError.{UnexpectedDbError, DbNotRespondingError, FailedDbQuery}
+import db.{handleDbError, handleUnfailableQuery}
 
 import com.augustnagro.magnum.magzio.*
-import zio.{RLayer, Task, IO, ZIO, ZLayer}
+import zio.{RLayer, IO, ZIO, ZLayer}
 
 trait StorageIngredientsRepo:
   def addIngredientToStorage(storageId: StorageId, ingredientId: IngredientId):
-    IO[StorageError.NotFound | IngredientError.NotFound, Unit]
+    IO[DbError, Unit]
 
   def removeIngredientFromStorageById(storageId: StorageId, ingredientId: IngredientId):
-    IO[StorageError.NotFound, Unit]
+    IO[DbError, Unit]
 
   def getAllIngredientsFromStorage(storageId: StorageId):
-    IO[DbError.UnexpectedDbError, Vector[IngredientId]]
+    IO[DbError, Vector[IngredientId]]
 
 final case class StorageIngredientsRepoLive(xa: Transactor)
   extends Repo[DbStorageIngredient, DbStorageIngredient, Null] with StorageIngredientsRepo:
 
   override def addIngredientToStorage(storageId: StorageId, ingredientId: IngredientId):
-    IO[StorageError.NotFound | IngredientError.NotFound, Unit] =
+    IO[DbError, Unit] =
     xa.transact(insert(DbStorageIngredient(storageId, ingredientId))).mapError {
-      _ => StorageError.NotFound(storageId) // TODO actual error handling
+      handleDbError
     }
 
   override def removeIngredientFromStorageById(storageId: StorageId, ingredientId: IngredientId):
-    IO[StorageError.NotFound, Unit] =
+    IO[DbError, Unit] =
     xa.transact(delete(DbStorageIngredient(storageId, ingredientId))).mapError {
-      _ => StorageError.NotFound(storageId) // TODO actual error handling
+      handleDbError
     }
 
   override def getAllIngredientsFromStorage(storageId: StorageId):
-    IO[DbError.UnexpectedDbError, Vector[IngredientId]] =
+    IO[DbError, Vector[IngredientId]] =
     xa.transact {
       sql"""
         SELECT ${storageIngredientsTable.ingredientId} FROM ${storageIngredientsTable}
         WHERE ${storageIngredientsTable.storageId} = $storageId
       """.query[IngredientId].run()
     }.mapError {
-      e => DbError.UnexpectedDbError(e.getMessage()) // TODO actual error handling
+      handleDbError
     }
 
 object StorageIngredientsRepoLive:
