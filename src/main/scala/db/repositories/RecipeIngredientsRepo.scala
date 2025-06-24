a9a7d47110c2bad6fcb3f3142c4c13b7b9691d82
TheBugYouCantFix <alex.gorbanev05@gmail.com>
2025-06-24 19:23:53 +0300

    feat: add suggested recipes method

--- src/main/scala/db/repositories/RecipeIngredientsRepo.scala
+++ src/main/scala/db/repositories/RecipeIngredientsRepo.scala
@@ -1,16 +1,20 @@
 package db.repositories
 
-import db.tables.RecipeIngredients
-import domain.{RecipeId, IngredientId, DbError}
-
+import db.tables.{RecipeIngredients, Recipes}
+import domain.{DbError, StorageError, IngredientId, RecipeId, StorageId}
 import com.augustnagro.magnum.magzio.*
-import zio.IO
+import zio.{IO, ZIO, ZLayer}
 
 trait RecipeIngredientsRepo:
+  protected type RecipeSummary = (RecipeId, String, Int, Int)
   def getAllIngredients(recipeId: RecipeId): IO[Err, Vector[IngredientId]]
   def addIngredients(recipeId: RecipeId, ingredientIds: Vector[IngredientId]): IO[Err, Unit]
   def deleteIngredient(recipeId: RecipeId, ingredientId: IngredientId): IO[Err, Unit]
-
+  def getSuggestedIngredients(
+                               size: Int,
+                               offset: Int,
+                               storageIds: Vector[StorageId]
+                             ): ZIO[StorageIngredientsRepo, Err | StorageError.NotFound, Vector[RecipeSummary]]
 final case class RecipeIngredientsRepoLive(xa: Transactor)
   extends Repo[RecipeIngredients, RecipeIngredients, (RecipeId, IngredientId)] with RecipeIngredientsRepo:
 
@@ -32,5 +36,52 @@ final case class RecipeIngredientsRepoLive(xa: Transactor)
       delete(RecipeIngredients(recipeId, ingredientId))
     }.catchAllAsDbError
 
+  override def getSuggestedIngredients(
+                               size: Int,
+                               offset: Int,
+                               storageIds: Vector[StorageId]
+                             ): ZIO[StorageIngredientsRepo, Err | StorageError.NotFound, Vector[RecipeSummary]] =
+    val table = RecipeIngredients.table
+    for
+      allIngredients<- ZIO.collectAll(storageIds.map(storageId =>
+        ZIO.serviceWithZIO[StorageIngredientsRepo](_.getAllIngredientsFromStorage(storageId))
+      )).map(_.flatten)
+      res <- xa.transact {
+        val frag =
+          sql"""
+            WITH recipe_stats AS (
+            SELECT
+              ${table.recipeId},
+              r.${Recipes.table.name} AS recipe_name,
+              COUNT(*) AS total_ingredients,
+              SUM(CASE WHEN ${table.ingredientId} IN (${allIngredients.mkString(",")}) THEN 1 ELSE 0 END) AS available_ingredients
+            FROM
+              $table ri
+            JOIN
+              ${Recipes.table} r ON r.${Recipes.table.id} = ri.${table.recipeId}
+            GROUP BY
+              ${table.recipeId}
+            )
+            SELECT
+              ${table.recipeId},
+              recipe_name,
+              available_ingredients,
+              total_ingredients,
+              (available_ingredients::float / total_ingredients) AS availability_ratio
+            FROM
+              recipe_stats
+            WHERE
+              total_ingredients > 0  -- Avoid division by zero
+            ORDER BY
+              availability_ratio DESC,
+              total_ingredients DESC
+            LIMIT $size
+            OFFSET $offset;
+            """
+
+        frag.query[RecipeSummary].run()
+      }.catchAllAsDbError
+    yield res
+    
 object RecipeIngredientsRepo:
   val layer = ZLayer.fromFunction(RecipeIngredientsRepoLive(_))
