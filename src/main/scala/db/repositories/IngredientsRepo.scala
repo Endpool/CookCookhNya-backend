6ae54b7565b8ba939641c35d87e8fecca1b33db9
Leropsis <v.ksenofontov@innopolis.university>
2025-06-25 01:54:16 +0300

    chore: add database error handling for ingredients repo

--- src/main/scala/db/repositories/IngredientsRepo.scala
+++ src/main/scala/db/repositories/IngredientsRepo.scala
@@ -1,39 +1,39 @@
 package db.repositories
 
 import db.tables.{DbIngredient, DbIngredientCreator}
-import domain.DbError
-import domain.{IngredientId}
-import domain.IngredientError.NotFound
+import domain.DbError.{UnexpectedDbError, DbNotRespondingError}
+import domain.IngredientId
+import db.{handleDbError, handleUnfailableQuery}
 
 import com.augustnagro.magnum.magzio.*
 import zio.{IO, RLayer, UIO, ZIO, ZLayer}
 
 trait IngredientsRepo:
-  def add(name: String): IO[DbError, DbIngredient]
-  def getById(id: IngredientId): IO[DbError, Option[DbIngredient]]
-  def removeById(id: IngredientId): IO[DbError, Unit]
-  def getAll: IO[DbError, Vector[DbIngredient]]
+  def add(name: String): IO[UnexpectedDbError | DbNotRespondingError, DbIngredient]
+  def getById(id: IngredientId): IO[UnexpectedDbError | DbNotRespondingError, Option[DbIngredient]]
+  def removeById(id: IngredientId): IO[UnexpectedDbError | DbNotRespondingError, Unit]
+  def getAll: IO[UnexpectedDbError | DbNotRespondingError, Vector[DbIngredient]]
 
 final case class IngredientsRepoLive(xa: Transactor)
   extends Repo[DbIngredientCreator, DbIngredient, IngredientId] with IngredientsRepo:
-  override def add(name: String): IO[DbError, DbIngredient] =
-    xa.transact(insertReturning(DbIngredientCreator(name))).catchAll { e =>
-      ZIO.fail(DbError.UnexpectedDbError(e.getMessage()))
+  override def add(name: String): IO[UnexpectedDbError | DbNotRespondingError, DbIngredient] =
+    xa.transact(insertReturning(DbIngredientCreator(name))).mapError {
+      e => handleUnfailableQuery(handleDbError(e))
     }
 
-  override def getById(id: IngredientId): IO[DbError, Option[DbIngredient]] =
-    xa.transact(findById(id)).catchAll { e =>
-      ZIO.fail(DbError.UnexpectedDbError(e.getMessage()))
+  override def getById(id: IngredientId): IO[UnexpectedDbError | DbNotRespondingError, Option[DbIngredient]] =
+    xa.transact(findById(id)).mapError {
+      e => handleUnfailableQuery(handleDbError(e))
     }
 
-  override def removeById(id: IngredientId): IO[DbError, Unit] =
-    xa.transact(deleteById(id)).catchAll { e =>
-      ZIO.fail(DbError.UnexpectedDbError(e.getMessage()))
+  override def removeById(id: IngredientId): IO[UnexpectedDbError | DbNotRespondingError, Unit] =
+    xa.transact(deleteById(id)).mapError {
+      e => handleUnfailableQuery(handleDbError(e))
     }
 
-  override def getAll: IO[DbError, Vector[DbIngredient]] =
-    xa.transact(findAll).catchAll { e =>
-      ZIO.fail(DbError.UnexpectedDbError(e.getMessage()))
+  override def getAll: IO[UnexpectedDbError | DbNotRespondingError, Vector[DbIngredient]] =
+    xa.transact(findAll).mapError {
+      e => handleUnfailableQuery(handleDbError(e))
     }
 
 object IngredientsRepoLive:
