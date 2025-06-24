0848dc866c070a200b835503ccff618cf4504a25
danielambda <daniel.gevorgyan25@gmail.com>
2025-06-24 23:11:40 +0300

    chore: added owner validation when dealing with storage members

--- src/main/scala/api/storages/members/Add.scala
+++ src/main/scala/api/storages/members/Add.scala
@@ -3,7 +3,7 @@ package api.storages.members
 import api.AppEnv
 import api.EndpointErrorVariants.{userNotFoundVariant, storageNotFoundVariant}
 import api.zSecuredServerLogic
-import db.repositories.StorageMembersRepo
+import db.repositories.{StorageMembersRepo, StoragesRepo}
 import domain.{UserError, StorageError, StorageId, UserId, DbError}
 import domain.StorageError.NotFound
 import domain.UserError.NotFound
@@ -12,6 +12,9 @@ import sttp.tapir.json.circe.*
 import sttp.tapir.ztapir.*
 import sttp.model.StatusCode
 import zio.ZIO
+import sttp.tapir.ValidationError
+import io.circe.{Encoder, Json}
+import io.circe.syntax.*
 
 private val add: ZServerEndpoint[AppEnv, Any] =
   storagesMembersEndpoint
@@ -22,7 +25,16 @@ private val add: ZServerEndpoint[AppEnv, Any] =
   .zSecuredServerLogic(addHandler)
 
 private def addHandler(userId: UserId)(storageId: StorageId, memberId: UserId):
-  ZIO[StorageMembersRepo, UserError.NotFound | StorageError.NotFound, Unit] =
-  ZIO.serviceWithZIO[StorageMembersRepo] {
-    _.addMemberToStorageById(storageId, memberId)
-  }
+  ZIO[StorageMembersRepo & StoragesRepo, DbError.UnexpectedDbError | UserError.NotFound | StorageError.NotFound, Unit] = for
+    mStorage <- ZIO.serviceWithZIO[StoragesRepo] {
+      _.getById(storageId)
+    }
+    ownerId <- ZIO.fromOption(mStorage)
+      .orElseFail[StorageError.NotFound](StorageError.NotFound(storageId))
+      .map(_.ownerId)
+    _ <- ZIO.when(ownerId == memberId) {
+      ZIO.serviceWithZIO[StorageMembersRepo] {
+        _.addMemberToStorageById(storageId, memberId)
+      }
+    }
+  yield ()
