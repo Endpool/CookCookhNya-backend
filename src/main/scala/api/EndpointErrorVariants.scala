c26a0bb02cf0b2d7483f7e5442c84c650c141533
TheBugYouCantFix <alex.gorbanev05@gmail.com>
2025-06-29 02:09:58 +0300

    refactor: add extension method to reduce boilerplate

--- src/main/scala/api/EndpointErrorVariants.scala
+++ src/main/scala/api/EndpointErrorVariants.scala
@@ -1,21 +1,24 @@
 package api
 
-import domain.{DbError, IngredientError, StorageError, UserError}
+import domain.{DbError, ErrorResponse, IngredientError, StorageError, UserError}
 import io.circe.generic.auto.*
+import io.circe.{Decoder, Encoder}
 import sttp.model.StatusCode
 import sttp.tapir.generic.auto.*
 import sttp.tapir.json.circe.*
 import sttp.tapir.ztapir.*
 
+import scala.reflect.ClassTag
+import sttp.model.StatusCode.*
+
+
+extension (sc: StatusCode)
+  def variantJson[E <: ErrorResponse : ClassTag] =
+    oneOfVariant(statusCode(sc).and(jsonBody[E]))
+
 object EndpointErrorVariants:
-  val ingredientNotFoundVariant =
-    oneOfVariant(statusCode(StatusCode.NotFound).and(jsonBody[IngredientError.NotFound]))
+  val ingredientNotFoundVariant  = NotFound.variantJson[IngredientError.NotFound]
+  val storageNotFoundVariant = NotFound.variantJson[StorageError.NotFound]
+  val userNotFoundVariant = NotFound.variantJson[UserError.NotFound]
 
-  val storageNotFoundVariant =
-    oneOfVariant(statusCode(StatusCode.NotFound).and(jsonBody[StorageError.NotFound]))
-
-  val userNotFoundVariant =
-    oneOfVariant(statusCode(StatusCode.NotFound).and(jsonBody[UserError.NotFound]))
-
-  val serverErrorVariant =
-    oneOfVariant(statusCode(StatusCode.InternalServerError).and(jsonBody[DbError.UnexpectedDbError]))
+  val serverErrorVariant = InternalServerError.variantJson[DbError.UnexpectedDbError]
