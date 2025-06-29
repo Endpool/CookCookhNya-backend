f988326f0de3a2e8ed04aadbcea1087ebab23968
Leropsis <v.ksenofontov@innopolis.university>
2025-06-29 20:04:43 +0300

    fix: replace hardcoded sql column names with magnum's table info

--- src/main/scala/api/DbErrorHandlers.scala
+++ src/main/scala/api/DbErrorHandlers.scala
@@ -4,6 +4,10 @@ import db.DbError.FailedDbQuery
 import domain.{IngredientError, IngredientId, InternalServerError, StorageError, StorageId, UserError, UserId}
 import zio.{IO, ZIO}
 
+import db.tables.*
+// import com.augustnagro.magnum.magzio.*
+
+
 def handleFailedSqlQuery(error: FailedDbQuery): IO[InternalServerError, String] =
   val pattern = """Key \((.*)\)=\((.*)\) is not present in table "(.*)".""".r
   error.sqlExc.getServerErrorMessage.getDetail match
@@ -11,16 +15,21 @@ def handleFailedSqlQuery(error: FailedDbQuery): IO[InternalServerError, String] 
     case _ => ZIO.fail(InternalServerError())
 
 def toIngredientNotFound(keyName: String, ingredientId: IngredientId):
-IO[IngredientError.NotFound, Unit] =
-  if keyName == "ingredient_id" then ZIO.fail(IngredientError.NotFound(ingredientId))
+IO[IngredientError.NotFound, Unit] = {
+  val sqlColName: String = storageIngredientsTable.ingredientId.sqlName
+  if keyName == sqlColName then ZIO.fail(IngredientError.NotFound(ingredientId))
   else ZIO.unit
+}
 
 def toStorageNotFound(keyName: String, storageId: StorageId):
   IO[StorageError.NotFound, Unit] =
-  if keyName == "storage_id" then ZIO.fail(StorageError.NotFound(storageId))
+  val sqlColName: String = storageMembersTable.storageId.sqlName
+  if keyName == sqlColName then ZIO.fail(StorageError.NotFound(storageId))
   else ZIO.unit
 
 def toUserNotFound(keyName: String, userId: UserId):
-  IO[UserError.NotFound, Unit] =
-  if keyName == "member_id" then ZIO.fail(UserError.NotFound(userId))
+  IO[UserError.NotFound, Unit] = {
+  val sqlColName: String = storageMembersTable.memberId.sqlName
+  if keyName == sqlColName then ZIO.fail(UserError.NotFound(userId))
   else ZIO.unit
+}
