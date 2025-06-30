5c05a399aed62d4ae40897b21891ce2a692bc31c
TheBugYouCantFix <alex.gorbanev05@gmail.com>
2025-06-30 16:46:27 +0300

    feat: add error variant

--- src/main/scala/api/EndpointErrorVariants.scala
+++ src/main/scala/api/EndpointErrorVariants.scala
@@ -1,6 +1,6 @@
 package api
 
-import domain.{DbError, ErrorResponse, IngredientError, StorageError, UserError}
+import domain.{DbError, ErrorResponse, IngredientError, RecipeError, StorageError, UserError}
 import io.circe.generic.auto.*
 import io.circe.{Decoder, Encoder}
 import sttp.model.StatusCode
@@ -12,14 +12,14 @@ import scala.reflect.ClassTag
 import sttp.model.StatusCode.*
 import sttp.tapir.EndpointOutput
 
-
 extension (sc: StatusCode)
   def variantJson[E <: ErrorResponse : ClassTag]: EndpointOutput.OneOfVariant[E] =
     oneOfVariant(statusCode(sc).and(jsonBody[E]))
-    
+
 object EndpointErrorVariants:
-  val ingredientNotFoundVariant  = NotFound.variantJson[IngredientError.NotFound] 
+  val ingredientNotFoundVariant  = NotFound.variantJson[IngredientError.NotFound]
   val storageNotFoundVariant = NotFound.variantJson[StorageError.NotFound] 
   val userNotFoundVariant = NotFound.variantJson[UserError.NotFound] 
+  val recipeNotFoundVariant = NotFound.variantJson[RecipeError.NotFound]
 
   val serverErrorVariant = InternalServerError.variantJson[DbError.UnexpectedDbError] 
