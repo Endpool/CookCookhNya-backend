9ad86966e0e45cab5e2cf5f25594b6b38d10e9ff
TheBugYouCantFix <alex.gorbanev05@gmail.com>
2025-07-02 01:45:58 +0300

    fix: bring back original getAll

--- src/main/scala/api/ingredients/GetAll.scala
+++ src/main/scala/api/ingredients/GetAll.scala
@@ -2,9 +2,9 @@ package api.ingredients
 
 import api.AppEnv
 import api.EndpointErrorVariants.serverErrorVariant
-import api.zSecuredServerLogic
 import db.repositories.IngredientsRepo
-import domain.{InternalServerError, UserId}
+import domain.InternalServerError
+
 import io.circe.generic.auto.*
 import sttp.model.StatusCode
 import sttp.tapir.generic.auto.*
@@ -14,14 +14,12 @@ import zio.ZIO
 
 val getAll: ZServerEndpoint[AppEnv, Any] =
   ingredientsEndpoint
-  .securityIn(auth.bearer[UserId]())
   .get
-  .out(jsonBody[Vector[IngredientResp]])
+  .out(jsonBody[Seq[IngredientResp]])
   .out(statusCode(StatusCode.Ok))
   .errorOut(oneOf(serverErrorVariant))
-  .zSecuredServerLogic(u => _ => getAllHandler(u))
+  .zServerLogic(_ => getAllHandler)
 
-def getAllHandler(userId: UserId):
-ZIO[AppEnv, InternalServerError, Vector[IngredientResp]] =
-  ZIO.serviceWithZIO[IngredientsRepo](_.getAll(userId)).map(_.map(IngredientResp.fromDb))
+val getAllHandler: ZIO[IngredientsRepo, InternalServerError, Seq[IngredientResp]] =
+  ZIO.serviceWithZIO[IngredientsRepo](_.getAll).map(_.map(IngredientResp.fromDb))
     .mapError(_ => InternalServerError())
