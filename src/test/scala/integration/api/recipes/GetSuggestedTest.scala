f35cf01910021131de225f200c89077757bb7f6c
danielambda <daniel.gevorgyan25@gmail.com>
2025-07-01 20:36:06 +0300

    refactor: organized test code

--- src/test/scala/integration/api/recipes/GetSuggestedTest.scala
+++ src/test/scala/integration/api/recipes/GetSuggestedTest.scala
@@ -1,24 +1,36 @@
+package integration.api.recipes
+
 import api.Main
 import api.recipes.getSuggested
-import com.augustnagro.magnum.magzio.sql
-import com.augustnagro.magnum.magzio.Transactor
-import com.dimafeng.testcontainers.PostgreSQLContainer
 import db.createTables
 import db.DataSourceDescription
 import db.dbLayer
 import db.repositories.*
+import domain.UserId
+
+import com.augustnagro.magnum.magzio.sql
+import com.augustnagro.magnum.magzio.Transactor
+import com.dimafeng.testcontainers.PostgreSQLContainer
 import zio.*
 import zio.http.*
+import zio.http.Request.*
 import zio.test.*
 import zio.test.Assertion.*
 
 case class ServerFiber(server: Fiber.Runtime[Throwable, Nothing])
 
+extension(req: Request)
+  def addAuthorization(userId: UserId) =
+    req.addHeader("Authorization", s"Bearer $userId")
+
 object IntegrationTest extends ZIOSpecDefault:
+  val serverPort = 8080
+  val serverUrl: URL = URL.decode(s"http://localhost:$serverPort/").toOption.get
+
   def server(dataSourceDescr: DataSourceDescription): ZIO[Any, Throwable, Nothing] =
     Server.serve(Main.app)
       .provide(
-        ZLayer.succeed(Server.Config.default.port(8080)),
+        ZLayer.succeed(Server.Config.default.port(serverPort)),
         dbLayer(dataSourceDescr),
         IngredientsRepoLive.layer,
         UsersRepo.layer,
@@ -32,29 +44,30 @@ object IntegrationTest extends ZIOSpecDefault:
 
   override def spec =
     test("get my storages returns status Ok") {
+      val userId = 52
       for
         resp <- Client.batched(
-          Request.get("http://localhost:8080/my/storages")
-            .addHeader("Authorization", "Bearer 52")
-        ).provide(Client.default)
+          get(serverUrl / "my" / "storages")
+            .addAuthorization(userId)
+        )
       yield assertTrue(resp.status == Status.Ok)
-    }.provideSomeLayerShared(serverLayer)
-     .provideSomeLayer(psqlContainerLayer)
+    } .provideSomeLayer(Client.default)
+      .provideSomeLayerShared(serverLayer)
+      .provideSomeLayer(psqlContainerLayer)
 
-  val psqlContainerLayer: ZLayer[Any, Throwable, PostgreSQLContainer] =
-    ZLayer.scoped {
-      ZIO.acquireRelease(
-        ZIO.attempt {
-          val container = PostgreSQLContainer()
-          container.start
-          container
-        }
-      ){ container =>
-        ZIO.attemptBlocking{ container.stop }.orDie
+  val psqlContainerLayer: TaskLayer[PostgreSQLContainer] = ZLayer.scoped {
+    ZIO.acquireRelease(
+      ZIO.attempt {
+        val container = PostgreSQLContainer()
+        container.start
+        container
       }
+    ){ container =>
+      ZIO.attemptBlocking{ container.stop }.orDie
     }
+  }
 
-  def serverLayer: ZLayer[PostgreSQLContainer, Nothing, ServerFiber] = ZLayer.scoped {
+  def serverLayer: URLayer[PostgreSQLContainer, ServerFiber] = ZLayer.scoped {
     for
       container <- ZIO.service[PostgreSQLContainer]
       dataSourceDescr = DataSourceDescription(
