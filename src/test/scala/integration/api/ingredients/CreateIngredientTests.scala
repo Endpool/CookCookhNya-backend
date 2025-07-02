97b54fa07223631fb5733d9f025a7950676a2489
TheBugYouCantFix <alex.gorbanev05@gmail.com>
2025-07-02 20:28:39 +0300

    fix: bruh

--- src/test/scala/integration/api/ingredients/CreateIngredientTests.scala
+++ src/test/scala/integration/api/ingredients/CreateIngredientTests.scala
@@ -1,19 +1,16 @@
 package integration.api.ingredients
 
-import api.ingredients.CreateIngredientReqBody
-import db.dbLayer
-import db.repositories.IngredientsRepo
-import db.repositories.StoragesRepo
+import api.storages.CreateStorageReqBody
 import integration.common.Utils.*
 import integration.common.ZIOIntegrationTestSpec
 import io.circe.generic.auto.*
 import zio.http.{Client, Status}
-import zio.{Scope, ZIO}
-import zio.test.Assertion.*
-import zio.test.{SmartAssertMacros, SmartAssertionOps, Spec, TestEnvironment, TestLensOptionOps, assert, assertTrue}
+import zio.Scope
+import zio.test.{Spec, TestEnvironment, assertTrue}
 
 object CreateIngredientTests extends ZIOIntegrationTestSpec:
   override def spec: Spec[TestEnvironment & Scope, Any] =
+    suite("Create ingredient tests")(
       test("Should result in 200") {
         for
           resp <- Client.batched(
