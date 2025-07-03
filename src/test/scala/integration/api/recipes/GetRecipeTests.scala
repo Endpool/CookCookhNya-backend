b9ec32d68021858c0db411835283503a137eea8f
TheBugYouCantFix <alex.gorbanev05@gmail.com>
2025-07-03 18:54:54 +0300

    feat: auth test

---
+++ src/test/scala/integration/api/recipes/GetRecipeTests.scala
@@ -0,0 +1,27 @@
+package integration.api.recipes
+
+import api.AppEnv
+import domain.{IngredientId, StorageId}
+import integration.common.Utils.*
+import integration.common.ZIOIntegrationTestSpec
+import api.recipes.{IngredientSummary, RecipeResp}
+import db.repositories.StorageIngredientsRepo
+import io.circe.parser.*
+import io.circe.generic.auto.*
+import zio.http.{Client, Request, Status}
+import zio.{Scope, ZIO}
+import zio.test.{SmartAssertionOps, Spec, TestEnvironment, TestLensOptionOps, assertTrue}
+
+object GetRecipeTests extends ZIOIntegrationTestSpec:
+  override def spec: Spec[TestEnvironment & Scope, Any] =
+    val defaultPath = "recipes/"
+    val defaultIngredientAmount = 5
+
+    suite("Get recipe (detailed) tests")(
+      test("When unauthorized should get 401") {
+        Client.batched(
+          Request.get(s"$defaultPath/1")
+        ).map(resp => assertTrue(resp.status == Status.Unauthorized))
+      },
+    ).provideLayer(testLayer)
+
