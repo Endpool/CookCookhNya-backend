311922e825c68c2363b4d09e769e2f85b9ab223d
TheBugYouCantFix <alex.gorbanev05@gmail.com>
2025-07-05 02:12:57 +0300

    refactor: use randomString for storage names

--- src/test/scala/integration/api/storages/DeleteStorageTests.scala
+++ src/test/scala/integration/api/storages/DeleteStorageTests.scala
@@ -36,7 +36,7 @@ object DeleteStorageTests extends ZIOIntegrationTestSpec:
     test("When user owns the storage should get 204 and storage should be deleted") {
       for
         userId <- registerUser
-        storageName <- storageNameGen.sample.map(_.value).runHead.some
+        storageName <- randomString
         storageId <- ZIO.serviceWithZIO[StoragesRepo] { _.createEmpty(storageName, userId) }
 
         resp <- Client.batched(
@@ -52,7 +52,7 @@ object DeleteStorageTests extends ZIOIntegrationTestSpec:
     test("When user is a member should get 403 and storage should not be deleted") {
       for
         creatorId <- registerUser
-        storageName <- storageNameGen.sample.map(_.value).runHead.some
+        storageName <- randomString
         storageId <- ZIO.serviceWithZIO[StoragesRepo] { _.createEmpty(storageName, creatorId) }
         userId <- registerUser
         _ <- ZIO.serviceWithZIO[StorageMembersRepo] { _.addMemberToStorageById(storageId, userId) }
@@ -69,7 +69,7 @@ object DeleteStorageTests extends ZIOIntegrationTestSpec:
     test("When user is neither the owner not a member should get 204 and storage should not be deleted") {
       for
         creatorId <- registerUser
-        storageName <- storageNameGen.sample.map(_.value).runHead.some
+        storageName <- randomString
         storageId <- ZIO.serviceWithZIO[StoragesRepo] { _.createEmpty(storageName, creatorId) }
         userId <- registerUser
 
@@ -83,8 +83,3 @@ object DeleteStorageTests extends ZIOIntegrationTestSpec:
          && assertTrue(storageExists)
     }
   ).provideLayer(testLayer)
-
-  val storageNameGen: Gen[Any, String] = for
-    base <- Gen.elements("Pantry", "Fridge", "Freezer", "Cupboard", "Shelf")
-    suffix <- Gen.elements("", "-1", "-2", "-Main", "-Backup")
-  yield base + suffix
