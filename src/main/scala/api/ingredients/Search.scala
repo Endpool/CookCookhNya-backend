5671b56f0e7e4913e9599c85e42755a5b82586c2
TheBugYouCantFix <alex.gorbanev05@gmail.com>
2025-07-10 18:53:44 +0300

    refactor: search ingredients

--- src/main/scala/api/ingredients/Search.scala
+++ src/main/scala/api/ingredients/Search.scala
@@ -1,11 +1,11 @@
 package api.ingredients
 
 import api.EndpointErrorVariants.serverErrorVariant
+import api.common.search.*
 import db.repositories.{IngredientsRepo, StorageIngredientsRepo}
 import domain.{IngredientId, InternalServerError, StorageId, UserId}
 
 import io.circe.generic.auto.*
-import me.xdrop.fuzzywuzzy.FuzzySearch
 import sttp.tapir.generic.auto.*
 import sttp.tapir.json.circe.*
 import sttp.tapir.ztapir.*
@@ -15,7 +15,7 @@ case class IngredientSearchResult(
   id: IngredientId,
   name: String,
   available: Boolean
-)
+) extends Searchable
 
 case class SearchResultsResp(
   results: Vector[IngredientSearchResult],
@@ -27,23 +27,18 @@ private type SearchEnv = IngredientsRepo & StorageIngredientsRepo
 private val search: ZServerEndpoint[SearchEnv, Any] =
   endpoint
     .get
-    .in("ingredients-for-storage")
-    .in(query[String]("query"))
+    .searchInput
     .in(query[StorageId]("storage-id"))
-    .in(query[Int]("size").default(2))
-    .in(query[Int]("offset").default(0))
-    .in(query[Int]("threshold").default(50))
     .out(jsonBody[SearchResultsResp])
     .errorOut(oneOf(serverErrorVariant))
-    .zServerLogic(searchHandler)
+    .zServerLogic {
+      case (q, s, o, t, storageId) => searchHandler(SearchParams(q, s, o, t), storageId)
+    }
 
 // TODO this should be authenticated
 private def searchHandler(
-  query: String,
-  storageId: StorageId,
-  size: Int,
-  offset: Int,
-  threshold: Int
+                           si: SearchParams,
+                           storageId: StorageId,
 ): ZIO[SearchEnv, InternalServerError, SearchResultsResp] =
   for
     allIngredients <- ZIO.serviceWithZIO[IngredientsRepo](_.getAll.mapError(_ => InternalServerError()))
@@ -53,15 +48,8 @@ private def searchHandler(
           .map(inStorage => IngredientSearchResult(ingredient.id, ingredient.name, inStorage))
           .mapError(_ => InternalServerError())
     }
-    res = allIngredientsAvailability
-      .map(i => (i, FuzzySearch.tokenSetPartialRatio(query, i.name)))
-      .filter((_, ratio) => ratio >= threshold)
-      .sortBy(
-        (i, ratio) => (
-          -ratio, // negate the ratio to make order descending
-          (i.name.length - query.length).abs // secondary sort by length difference
-        )
-      )
-      .map(_._1)
+    res = Searchable.search(
+      allIngredientsAvailability, si.query, si.size, si.offset, si.threshold
+    )
 
-  yield SearchResultsResp(res.slice(offset, offset + size), res.length)
+  yield SearchResultsResp(res.slice(si.offset, si.offset + si.size), res.length)
