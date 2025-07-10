a61a42dd0f33e162e1b246d217bb0830e42761e1
TheBugYouCantFix <alex.gorbanev05@gmail.com>
2025-07-10 18:54:47 +0300

    feat: search all recipes

---
+++ src/main/scala/api/recipes/SearchAll.scala
@@ -0,0 +1,35 @@
+package api.recipes
+
+import api.EndpointErrorVariants.serverErrorVariant
+import api.common.search.*
+import db.repositories.{RecipesRepo, StorageIngredientsRepo}
+import domain.InternalServerError
+import io.circe.generic.auto.*
+import sttp.tapir.generic.auto.*
+import sttp.tapir.json.circe.*
+import sttp.tapir.ztapir.*
+import zio.ZIO
+
+case class RecipeSearchResp(name: String, sourceLink: String) extends Searchable
+case class SearchAllRecipesResp(
+                                 results: Vector[RecipeSearchResp],
+                                 found: Int
+                               )
+
+private type SearchAllEnv = RecipesRepo & StorageIngredientsRepo
+
+private val searchAll: ZServerEndpoint[SearchAllEnv, Any] =
+  recipesEndpoint
+    .get
+    .searchInput
+    .out(jsonBody[SearchAllRecipesResp])
+    .errorOut(oneOf(serverErrorVariant))
+    .zServerLogic(searchAllRecipesHandler.inputToSearchParams)
+
+private def searchAllRecipesHandler(sp: SearchParams):
+  ZIO[SearchAllEnv, InternalServerError, SearchAllRecipesResp] =
+  for
+    allDbRecipes <- ZIO.serviceWithZIO[RecipesRepo] (_.getAll.mapError(_ => InternalServerError()))
+    allRecipes= allDbRecipes.map(dbRecipe => RecipeSearchResp(dbRecipe.name, dbRecipe.sourceLink))
+    res = Searchable.search(allRecipes, sp.query, sp.size, sp.offset, sp.threshold)
+  yield SearchAllRecipesResp(res.slice(sp.offset, sp.offset + sp.size), res.length)
