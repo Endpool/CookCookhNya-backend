84df4bb6771f8702e92ee0ca9325e1c6ea395626
danielambda <daniel.gevorgyan25@gmail.com>
2025-07-11 03:05:37 +0300

    test: When create valid recipe, recipe should be added to db

--- src/test/scala/integration/api/recipes/CreateRecipeTests.scala
+++ src/test/scala/integration/api/recipes/CreateRecipeTests.scala
@@ -1,9 +1,10 @@
 package integration.api.recipes
 
-import api.recipes.{IngredientSummary, RecipeResp}
 import api.Authentication.AuthenticatedUser
-import db.repositories.{StorageIngredientsRepo, StorageMembersRepo}
-import domain.{IngredientId, StorageId}
+import api.recipes.CreateRecipeReqBody
+import api.recipes.{RecipeResp}
+import db.repositories.{IngredientsRepo, RecipesRepo}
+import domain.{IngredientId}
 import integration.common.Utils.*
 import integration.common.ZIOIntegrationTestSpec
 
@@ -11,9 +12,14 @@ import io.circe.parser.*
 import io.circe.generic.auto.*
 import zio.http.{Client, Path, Request, Status, URL}
 import zio.{Scope, ZIO, RIO}
-import zio.test.{Spec, TestEnvironment, assertTrue}
 import zio.http.Response
-import api.recipes.CreateRecipeReqBody
+import zio.test.{
+  Gen,
+  TestEnvironment,
+  assertTrue,
+  Spec,
+  SmartAssertionOps, TestLensOptionOps
+}
 
 object CreateRecipeTests extends ZIOIntegrationTestSpec:
   private val endpointPath = URL(Path.root / "recipes")
@@ -38,6 +44,24 @@ object CreateRecipeTests extends ZIOIntegrationTestSpec:
         user <- registerUser
         resp <- createRecipe(user, CreateRecipeReqBody("recipe", "sourceLink", Vector.empty))
       yield assertTrue(resp.status == Status.Ok)
+    },
+    test("When create valid recipe, recipe should be added to db") {
+      for
+        user <- registerUser
+        recipeName <- randomString
+        recipeSourceLink <- randomString
+        ingredientIds <- ZIO.serviceWithZIO[IngredientsRepo](repo =>
+          Gen.string.runCollectN(10).flatMap(ZIO.foreach(_)(repo.add))
+        ).map(_.map(_.id))
+          .map(Vector.from)
+
+        resp <- createRecipe(user, CreateRecipeReqBody(recipeName, recipeSourceLink, ingredientIds))
+        recipeId <- resp.body.asString.map(_.replaceAll("\"", "").toUUID)
+        recipe <- ZIO.serviceWithZIO[RecipesRepo](_.getRecipe(recipeId))
+      yield assertTrue(resp.status == Status.Ok)
+         && assertTrue(recipe.is(_.some).name == recipeName)
+         && assertTrue(recipe.is(_.some).sourceLink == recipeSourceLink)
+         && assertTrue(recipe.get.ingredients hasSameElementsAs ingredientIds)
     },
   ).provideLayer(testLayer)
 
