62186ae79f57db6c647d1416ae68a3488a959ad2
danielambda <daniel.gevorgyan25@gmail.com>
2025-07-13 07:17:17 +0300

    chore: removed ErrorResponse supertype to improve type inference

--- src/main/scala/domain/Errors.scala
+++ src/main/scala/domain/Errors.scala
@@ -1,23 +1,20 @@
 package domain
 
-sealed trait ErrorResponse:
-  val message: String
-
-final case class IngredientNotFound(ingredientId: String) extends ErrorResponse:
+final case class IngredientNotFound(ingredientId: String):
   val message = s"No ingredient with id $ingredientId"
 
-final case class StorageNotFound(storageId: String) extends ErrorResponse:
+final case class StorageNotFound(storageId: String):
   val message = s"No storage with id $storageId"
-final case class StorageAccessForbidden(storageId: String) extends ErrorResponse:
+final case class StorageAccessForbidden(storageId: String):
   val message = s"Access to storage with id $storageId is forbidden"
 
-final case class UserNotFound(userId: String) extends ErrorResponse:
+final case class UserNotFound(userId: String):
   val message = s"No user with id $userId"
 
-final case class RecipeNotFound(recipeId: String) extends ErrorResponse:
+final case class RecipeNotFound(recipeId: String):
   val message = s"No recipe with id $recipeId"
 
-final case class InternalServerError(message: String = "Something went wrong on the server side") extends ErrorResponse
+final case class InternalServerError(message: String = "Something went wrong on the server side")
 
-final case class InvalidInvitationHash(hash: String) extends ErrorResponse:
+final case class InvalidInvitationHash(hash: String):
   val message = s"Invalid invitation hash: $hash"
