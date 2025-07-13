1873324d9b98d9f4fde39b43e840a39b880782dc
danielambda <daniel.gevorgyan25@gmail.com>
2025-07-13 07:16:00 +0300

    fix: make database initializtion idempotent

--- src/main/scala/db/tables/DbPublicationRequestStatus.scala
+++ src/main/scala/db/tables/DbPublicationRequestStatus.scala
@@ -19,11 +19,16 @@ object DbPublicationRequestStatus:
     case PublicationRequestStatus.Rejected(reason) => (Some(reason), Rejected)
 
   val createType: String = """
+  DO $$
+  BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'publication_request_status') THEN
     CREATE TYPE publication_request_status AS ENUM (
       'pending',
       'accepted',
       'rejected'
     );
+    END IF;
+  END $$;
   """
 
   given JdbcDecoder[DbPublicationRequestStatus](
