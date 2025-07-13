bcc0c72db7b107bcc42aec50975d097ff1e94e67
TheBugYouCantFix <alex.gorbanev05@gmail.com>
2025-07-13 21:55:52 +0300

    refactor: replace zio foreach w quill foreach

--- src/main/scala/db/repositories/ShoppingListsRepo.scala
+++ src/main/scala/db/repositories/ShoppingListsRepo.scala
@@ -3,15 +3,16 @@ package db.repositories
 import api.Authentication.AuthenticatedUser
 import db.tables.{DbShoppingList, shoppingListTable}
 import db.{DbError, handleDbError}
-import domain.{IngredientId, UserId}
+import domain.{IngredientId, StorageId, UserId}
 import db.QuillConfig.provideDS
+import db.repositories.StorageIngredientsQueries.addIngredientToStorageQ
 
 import javax.sql.DataSource
 import com.augustnagro.magnum.magzio.*
 import io.getquill.*
 import db.QuillConfig.ctx.*
 import db.repositories.ShoppingListsQueries.*
-import zio.{ZIO, ZLayer}
+import zio.{IO, ZIO, ZLayer}
 
 trait ShoppingListsRepo:
   def addIngredient(ingredientId: IngredientId): ZIO[AuthenticatedUser, DbError, Unit]
@@ -20,40 +21,51 @@ trait ShoppingListsRepo:
   def getIngredients: ZIO[AuthenticatedUser, DbError, Vector[IngredientId]]
 
   def deleteIngredient(ingredientId: IngredientId): ZIO[AuthenticatedUser, DbError, Unit]
-  def deleteIngredients(ingredientIds: Seq[IngredientId]): ZIO[AuthenticatedUser, DbError, Unit]
 
 private final case class ShoppingListsLive(xa: Transactor, dataSource: DataSource)
   extends Repo[DbShoppingList, DbShoppingList, Null] with ShoppingListsRepo:
 
   private given DataSource = dataSource
   def addIngredient(ingredientId: IngredientId): ZIO[AuthenticatedUser, DbError, Unit] =
-    for 
+    for
       ownerId <- ZIO.serviceWith[AuthenticatedUser](_.userId)
       res <- run(addIngredientQ(lift(ownerId), lift(ingredientId))).unit.provideDS
-    yield res 
+    yield res
 
-  override def addIngredients(ingredientIds: Seq[IngredientId]):
-    ZIO[AuthenticatedUser, DbError, Unit] = ZIO.foreachParDiscard(ingredientIds)(addIngredient) 
+  override def addIngredients(ingredientIds: Seq[IngredientId]):ZIO[AuthenticatedUser, DbError, Unit] =
+    for
+      ownerId <- ZIO.serviceWith[AuthenticatedUser](_.userId)
+      _ <- run(liftQuery(ingredientIds).foreach(
+        addIngredientQ(lift(ownerId), _)
+      )).provideDS
+    yield ()
 
   override def getIngredients: ZIO[AuthenticatedUser, DbError, List[IngredientId]] = for
     ownerId <- ZIO.serviceWith[AuthenticatedUser](_.userId)
     res <- run(getIngredientsQ(lift(ownerId))).provideDS
   yield res
 
-  override def deleteIngredient(ingredientId: IngredientId): ZIO[AuthenticatedUser, DbError, Unit] =
-    run(deleteIngredientQ(lift(ingredientId))).unit.provideDS
+  override def deleteIngredient(userId: UserId, ingredientId: IngredientId): ZIO[AuthenticatedUser, DbError, Unit] =
+    run(deleteIngredientQ(lift(userId), lift(ingredientId))).unit.provideDS
 
   override def deleteIngredients(ingredientIds: Seq[IngredientId]):
     ZIO[AuthenticatedUser, DbError, Unit] = ZIO.foreachParDiscard(ingredientIds)(deleteIngredient)
+  override def buyIngredientsToStorage(ingredientIds: Seq[IngredientId], storageId: StorageId):
+    for
+      userId <- ZIO.serviceWith[AuthenticatedUser](_.userId)
+      _ <- run(liftQuery(ingredientIds).foreach(deleteIngredientQ(userId, _)))
+      _ <- run(liftQuery(ingredientIds).foreach(addIngredientToStorageQ(storageId, _)))
+    yield ()
+  }.provideDS
 
 object ShoppingListsQueries:
-  inline def addIngredientQ(inline userId: UserId, inline ingredientId: IngredientId): Unit =
+  inline def addIngredientQ(inline userId: UserId, inline ingredientId: IngredientId) =
     query[DbShoppingList]
       .insertValue(DbShoppingList(userId, ingredientId))
-      
-  inline def deleteIngredientQ(inline ingredientId: IngredientId): Delete[DbShoppingList] =
+
+  inline def deleteIngredientQ(inline userId: UserId, inline ingredientId: IngredientId): Delete[DbShoppingList] =
     query[DbShoppingList]
-      .filter(_.ingredientId == ingredientId)
+      .filter(sl => sl.ownerId == userId && sl.ingredientId == ingredientId)
       .delete
 
   inline def getIngredientsQ(inline ownerId: UserId): EntityQuery[IngredientId] =
