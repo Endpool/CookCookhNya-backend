3d669ee5088cbbb89062633b6d7a436608c03490
danielambda <daniel.gevorgyan25@gmail.com>
2025-07-13 07:18:18 +0300

    feat: properly restrict ingredients usage in recipes creation

--- src/main/scala/api/recipes/Create.scala
+++ src/main/scala/api/recipes/Create.scala
@@ -4,14 +4,19 @@ import api.Authentication.{AuthenticatedUser, zSecuredServerLogic}
 import api.EndpointErrorVariants.{ingredientNotFoundVariant, serverErrorVariant}
 import api.{toIngredientNotFound, handleFailedSqlQuery}
 import db.DbError.{DbNotRespondingError, FailedDbQuery}
+import db.QuillConfig.ctx.*
+import db.QuillConfig.provideDS
 import db.repositories.{RecipeIngredientsRepo, RecipesRepo}
 import domain.{IngredientNotFound, IngredientId, InternalServerError, RecipeId}
 
+import io.getquill.*
 import io.circe.generic.auto.*
+import javax.sql.DataSource
 import sttp.tapir.generic.auto.*
 import sttp.tapir.json.circe.*
 import sttp.tapir.ztapir.*
 import zio.ZIO
+import db.repositories.IngredientsQueries
 
 final case class CreateRecipeReqBody(
   name: String,
@@ -19,7 +24,7 @@ final case class CreateRecipeReqBody(
   ingredients: List[IngredientId]
 )
 
-private type CreateEnv = RecipesRepo & RecipeIngredientsRepo
+private type CreateEnv = RecipesRepo & RecipeIngredientsRepo & DataSource
 
 private val create: ZServerEndpoint[CreateEnv, Any] =
   recipesEndpoint
@@ -30,8 +35,19 @@ private val create: ZServerEndpoint[CreateEnv, Any] =
     .zSecuredServerLogic(createHandler)
 
 private def createHandler(recipe: CreateRecipeReqBody):
-  ZIO[AuthenticatedUser & CreateEnv, InternalServerError | IngredientNotFound, RecipeId] =
-  ZIO.serviceWithZIO[RecipesRepo](_
+  ZIO[AuthenticatedUser & CreateEnv, InternalServerError | IngredientNotFound, RecipeId] = for
+  userId <- ZIO.serviceWith[AuthenticatedUser](_.userId)
+  dataSource <- ZIO.service[DataSource]
+  existingIngredientIds <- run(
+    IngredientsQueries.getAllVisibleQ(lift(userId))
+      .map(_.id)
+      .filter(id => liftQuery(recipe.ingredients).contains(id))
+  ).provideDS(using dataSource).orElseFail(InternalServerError())
+  uknownIngredientIds = recipe.ingredients.diff(existingIngredientIds)
+  _ <- ZIO.fail(IngredientNotFound(uknownIngredientIds.head.toString))
+    .when(uknownIngredientIds.nonEmpty)
+
+  recipeId <- ZIO.serviceWithZIO[RecipesRepo](_
     .addRecipe(recipe.name, recipe.sourceLink, recipe.ingredients)
     .mapError {
       case DbNotRespondingError(_) => InternalServerError()
@@ -40,3 +56,4 @@ private def createHandler(recipe: CreateRecipeReqBody):
         .getOrElse(InternalServerError())
     }
   )
+yield recipeId
