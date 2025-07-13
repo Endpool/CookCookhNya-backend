af078ac4d2e8c674268a7174d62220732ffe2324
danielambda <daniel.gevorgyan25@gmail.com>
2025-07-13 19:18:31 +0300

    refactor: use Pagination and SearchParams in ingredients search

--- src/main/scala/api/ingredients/Search.scala
+++ src/main/scala/api/ingredients/Search.scala
@@ -1,15 +1,15 @@
 package api.ingredients
 
 import api.Authentication.{zSecuredServerLogic, AuthenticatedUser}
+import api.common.search.{PaginationParams, paginate, SearchParams, Searchable}
 import api.EndpointErrorVariants.serverErrorVariant
 import db.repositories.{IngredientsRepo, StorageIngredientsRepo}
 import domain.{IngredientId, InternalServerError}
 
 import io.circe.generic.auto.*
-import me.xdrop.fuzzywuzzy.FuzzySearch
 import sttp.tapir.generic.auto.*
 import sttp.tapir.json.circe.*
-import sttp.tapir.ztapir.{query, *}
+import sttp.tapir.ztapir.*
 import zio.ZIO
 
 private type SearchAllEnv = IngredientsRepo & StorageIngredientsRepo
@@ -17,31 +17,18 @@ private type SearchAllEnv = IngredientsRepo & StorageIngredientsRepo
 private val search: ZServerEndpoint[SearchAllEnv, Any] =
   ingredientsEndpoint
     .get
-    .in(query[String]("query"))
-    .in(query[Int]("size").default(2))
-    .in(query[Int]("offset").default(0))
-    .in(query[Int]("threshold").default(50))
+    .in(SearchParams.query)
+    .in(PaginationParams.query)
     .out(jsonBody[SearchResp[IngredientResp]])
     .errorOut(oneOf(serverErrorVariant))
     .zSecuredServerLogic(searchHandler)
 
 private def searchHandler(
-  query: String,
-  size: Int,
-  offset: Int,
-  threshold: Int
+  searchParams: SearchParams,
+  paginationParams: PaginationParams,
 ): ZIO[AuthenticatedUser & SearchAllEnv, InternalServerError, SearchResp[IngredientResp]] =
   for
     allDbIngredients <- ZIO.serviceWithZIO[IngredientsRepo](_.getAllPersonal.orElseFail(InternalServerError()))
     allIngredients = allDbIngredients.map(IngredientResp.fromDb)
-    res = Vector.from(allIngredients)
-      .map(i => (i, FuzzySearch.tokenSetPartialRatio(query, i.name)))
-      .filter((_, ratio) => ratio >= threshold)
-      .sortBy(
-        (i, ratio) => (
-          -ratio, // negate the ratio to make order descending
-          (i.name.length - query.length).abs // secondary sort by length difference
-        )
-      )
-      .map(_._1)
-  yield SearchResp(res.slice(offset, offset + size), res.length)
+    res = Searchable.search(Vector.from(allIngredients), searchParams)
+  yield SearchResp(res.paginate(paginationParams), res.length)
