d9ebe88933b9408ea7b124e6256f525792403390
TheBugYouCantFix <alex.gorbanev05@gmail.com>
2025-07-14 02:37:24 +0300

    feat: buy endpoint

---
+++ src/main/scala/api/shoppinglist/Buy.scala
@@ -0,0 +1,32 @@
+package api.shoppinglist
+
+import api.{handleFailedSqlQuery, toIngredientNotFound}
+import api.Authentication.{AuthenticatedUser, zSecuredServerLogic}
+import api.EndpointErrorVariants.{ingredientNotFoundVariant, serverErrorVariant}
+import domain.{IngredientId, IngredientNotFound, InternalServerError, StorageId}
+import db.repositories.ShoppingListsRepo
+import db.DbError.*
+import sttp.model.StatusCode
+import sttp.tapir.ztapir.*
+import zio.ZIO
+
+private type BuyEnv = ShoppingListsRepo
+private val buy: ZServerEndpoint[BuyEnv, Any] =
+  shoppingListEndpoint
+    .put
+    .in("buy")
+    .in(query[StorageId]("storage-id"))
+    .in(query[List[IngredientId]]("ingredient-id"))
+    .out(statusCode(StatusCode.NoContent))
+    .errorOut(oneOf(serverErrorVariant, ingredientNotFoundVariant))
+    .zSecuredServerLogic(buyHandler)
+
+def buyHandler(storageId: StorageId, ingredientIds: List[IngredientId]):
+  ZIO[AuthenticatedUser & BuyEnv, InternalServerError | IngredientNotFound, Unit] = ZIO.serviceWithZIO[ShoppingListsRepo](
+    _.buyIngredientsToStorage(ingredientIds, storageId)
+  ).mapError {
+    case _: DbNotRespondingError => InternalServerError()
+    case e: FailedDbQuery => handleFailedSqlQuery(e)
+      .flatMap(toIngredientNotFound)
+      .getOrElse(InternalServerError())
+  }
