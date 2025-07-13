0c0f7f4e80101ee251f31fa891e7242ec9364500
danielambda <daniel.gevorgyan25@gmail.com>
2025-07-13 07:40:17 +0300

    refactor: restructured error messages

--- src/main/scala/domain/Errors.scala
+++ src/main/scala/domain/Errors.scala
@@ -1,20 +1,34 @@
 package domain
 
-final case class IngredientNotFound(ingredientId: String):
-  val message = s"No ingredient with id $ingredientId"
+final case class IngredientNotFound(
+  ingredientId: String,
+  message: String = "Ingredient not found",
+)
 
-final case class StorageNotFound(storageId: String):
-  val message = s"No storage with id $storageId"
-final case class StorageAccessForbidden(storageId: String):
-  val message = s"Access to storage with id $storageId is forbidden"
+final case class StorageNotFound(
+  storageId: String,
+  message: String = "Storage not found",
+)
+final case class StorageAccessForbidden(
+  storageId: String,
+  message: String = "Access to the storage is forbidden",
+)
 
-final case class UserNotFound(userId: String):
-  val message = s"No user with id $userId"
+final case class UserNotFound(
+  userId: String,
+  message: String = "User not found",
+)
 
-final case class RecipeNotFound(recipeId: String):
-  val message = s"No recipe with id $recipeId"
+final case class RecipeNotFound(
+  recipeId: String,
+  message: String = "Recipe not found",
+)
 
-final case class InternalServerError(message: String = "Something went wrong on the server side")
+final case class InternalServerError(
+  message: String = "Something went wrong on the server side",
+)
 
-final case class InvalidInvitationHash(hash: String):
-  val message = s"Invalid invitation hash: $hash"
+final case class InvalidInvitationHash(
+  hash: String,
+  message: String = "Invalid invitation hash",
+)
