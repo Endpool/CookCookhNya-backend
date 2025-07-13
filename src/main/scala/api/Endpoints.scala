f13c2f5a07495682dfae525ad9353c959aa27cf6
Rashid Badamshin <badamshinrashid@google.com>
2025-07-13 13:01:41 +0300

    add /metrics endpoints inisialisation

--- src/main/scala/api/Endpoints.scala
+++ src/main/scala/api/Endpoints.scala
@@ -7,14 +7,24 @@ import api.recipes.recipeEndpoints
 import api.shoppinglist.shoppingListEndpoints
 import api.invitations.invitationEndpoints
 
+import sttp.tapir.metrics.prometheus.PrometheusMetrics 
 import sttp.tapir.ztapir.ZServerEndpoint
 import sttp.tapir.ztapir.RichZServerEndpoint
+import zio.Task 
 
 object AppEndpoints:
-  val endpoints: List[ZServerEndpoint[AppEnv, Any]]
-    =  storageEndpoints.map(_.widen)
-    ++ ingredientsEndpoints.map(_.widen)
-    ++ usersEndpoints.map(_.widen)
-    ++ recipeEndpoints.map(_.widen)
-    ++ shoppingListEndpoints.map(_.widen)
-    ++ invitationEndpoints.map(_.widen)
+  // Initialize Prometheus metrics
+  private val metrics = PrometheusMetrics.default[Task]()
+
+  // Initialise API endpoints
+  private val apiEndpoints: List[ZServerEndpoint[AppEnv, Any]] =
+    storageEndpoints.map(_.widen) ++
+    ingredientsEndpoints.map(_.widen) ++
+    usersEndpoints.map(_.widen) ++
+    recipeEndpoints.map(_.widen) ++
+    shoppingListEndpoints.map(_.widen) ++
+    invitationEndpoints.map(_.widen)
+
+  // Expose the whole list of endpoints
+  val endpoints: List[ZServerEndpoint[AppEnv, Any]] =
+    apiEndpoints :+ metrics.metricsEndpoint.widen[AppEnv]
