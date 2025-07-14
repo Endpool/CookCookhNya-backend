e9849a72763fec70463b3d1ec26e366f1af6d88d
TheBugYouCantFix <alex.gorbanev05@gmail.com>
2025-07-14 03:58:57 +0300

    feat: add storage not found error

--- src/main/scala/api/shoppinglist/Buy.scala
+++ src/main/scala/api/shoppinglist/Buy.scala
@@ -1,9 +1,9 @@
 package api.shoppinglist
 
-import api.{handleFailedSqlQuery, toIngredientNotFound}
+import api.{handleFailedSqlQuery, toIngredientNotFound, toStorageNotFound}
 import api.Authentication.{AuthenticatedUser, zSecuredServerLogic}
-import api.EndpointErrorVariants.{ingredientNotFoundVariant, serverErrorVariant}
-import domain.{IngredientId, IngredientNotFound, InternalServerError, StorageId}
+import api.EndpointErrorVariants.{ingredientNotFoundVariant, serverErrorVariant, storageNotFoundVariant}
+import domain.{IngredientId, IngredientNotFound, InternalServerError, StorageId, StorageNotFound}
 import db.repositories.ShoppingListsRepo
 import db.DbError.*
 import sttp.model.StatusCode
@@ -18,15 +18,18 @@ private val buy: ZServerEndpoint[BuyEnv, Any] =
     .in(query[StorageId]("storage-id"))
     .in(query[List[IngredientId]]("ingredient-id"))
     .out(statusCode(StatusCode.NoContent))
-    .errorOut(oneOf(serverErrorVariant, ingredientNotFoundVariant))
+    .errorOut(oneOf(serverErrorVariant, ingredientNotFoundVariant, storageNotFoundVariant))
     .zSecuredServerLogic(buyHandler)
 
 def buyHandler(storageId: StorageId, ingredientIds: List[IngredientId]):
-  ZIO[AuthenticatedUser & BuyEnv, InternalServerError | IngredientNotFound, Unit] = ZIO.serviceWithZIO[ShoppingListsRepo](
+  ZIO[AuthenticatedUser & BuyEnv, InternalServerError | IngredientNotFound | StorageNotFound, Unit] = ZIO.serviceWithZIO[ShoppingListsRepo](
     _.buyIngredientsToStorage(ingredientIds, storageId)
   ).mapError {
     case _: DbNotRespondingError => InternalServerError()
     case e: FailedDbQuery => handleFailedSqlQuery(e)
       .flatMap(toIngredientNotFound)
+      .orElse(
+        handleFailedSqlQuery(e).flatMap(toStorageNotFound)
+      )
       .getOrElse(InternalServerError())
   }
