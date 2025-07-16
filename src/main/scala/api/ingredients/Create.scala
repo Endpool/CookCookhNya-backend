26c94c717204b3d5b032c7a0bae14ef6b4612f5e
TheBugYouCantFix <alex.gorbanev05@gmail.com>
2025-07-16 18:02:22 +0300

    feat: add userNotFound check to ingredient creation

--- src/main/scala/api/ingredients/Create.scala
+++ src/main/scala/api/ingredients/Create.scala
@@ -2,8 +2,13 @@ package api.ingredients
 
 import api.Authentication.{AuthenticatedUser, zSecuredServerLogic}
 import api.EndpointErrorVariants.{serverErrorVariant, userNotFoundVariant}
-import db.repositories.{IngredientsRepo, UsersRepo}
+import db.repositories.{IngredientsRepo, UserQueries}
 import domain.{IngredientId, InternalServerError, UserNotFound}
+import db.QuillConfig.provideDS
+import db.QuillConfig.ctx.*
+
+import javax.sql.DataSource
+import io.getquill.*
 import io.circe.generic.auto.*
 import sttp.model.StatusCode
 import sttp.tapir.generic.auto.*
@@ -11,7 +16,8 @@ import sttp.tapir.json.circe.*
 import sttp.tapir.ztapir.*
 import zio.ZIO
 
-private type CreateEnv = IngredientsRepo & UsersRepo
+
+private type CreateEnv = IngredientsRepo & DataSource
 
 private val create: ZServerEndpoint[CreateEnv, Any] =
   ingredientsEndpoint
@@ -19,16 +25,25 @@ private val create: ZServerEndpoint[CreateEnv, Any] =
     .in(jsonBody[CreateIngredientReqBody])
     .out(plainBody[IngredientId])
     .out(statusCode(StatusCode.Created))
-    .errorOut(oneOf(serverErrorVariant))
+    .errorOut(oneOf(serverErrorVariant, userNotFoundVariant))
     .zSecuredServerLogic(createHandler)
 
 private def createHandler(reqBody: CreateIngredientReqBody):
-  ZIO[AuthenticatedUser & CreateEnv, InternalServerError | UserNotFound, IngredientId] = for
-  userId <- ZIO.serviceWith[AuthenticatedUser](_.userId)
-  userNotFound <- ZIO.serviceWithZIO[UsersRepo](_.) 
-  ZIO.serviceWithZIO[IngredientsRepo](_
-    .addCustom(reqBody.name)
-    .map(_.id)
-    .orElseFail(InternalServerError())
-  )
+  ZIO[AuthenticatedUser & CreateEnv, InternalServerError | UserNotFound, IngredientId] =
+  for
+    userId <- ZIO.serviceWith[AuthenticatedUser](_.userId)
+    dataSource <- ZIO.service[DataSource]
+    userNotFound <- run(UserQueries.getUserById(lift(userId)).isEmpty)
+      .provideDS(using dataSource)
+      .orElseFail(InternalServerError())
+    _ <- ZIO.fail(UserNotFound(userId.toString))
+      .when(userNotFound)
+    
+    ingredientId <- ZIO.serviceWithZIO[IngredientsRepo](_
+      .addCustom(reqBody.name)
+      .map(_.id)
+      .orElseFail(InternalServerError())
+    )
+  yield ingredientId
+
 
