d697e60d522d35f2c5927986850b23b49b624478
TheBugYouCantFix <alex.gorbanev05@gmail.com>
2025-07-21 01:56:47 +0300

    refactor: rm ownerId

--- src/main/scala/api/storages/StorageSummaryResp.scala
+++ src/main/scala/api/storages/StorageSummaryResp.scala
@@ -3,8 +3,8 @@ package api.storages
 import db.tables.DbStorage
 import domain.{StorageId, UserId}
 
-final case class StorageSummaryResp(id: StorageId, ownerId: UserId, name: String)
+final case class StorageSummaryResp(id: StorageId, name: String)
 
 object StorageSummaryResp:
   def fromDb(dbStorage: DbStorage): StorageSummaryResp =
-    StorageSummaryResp(dbStorage.id, dbStorage.ownerId, dbStorage.name)
+    StorageSummaryResp(dbStorage.id, dbStorage.name)
